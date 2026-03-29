0e4a41e3c49eb6ddda1761e633a7e5b4c8f34a05
James Balazs <j.c.balazs1@gmail.com>
2026-03-30 00:31:20 +1100

    ci: block breaking changes since v2 tags are not valid for go modules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,30 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+        "breaking-change-forbidden": [2, "always"],
+        "type-enum": [2, "never", ["!"]], // doesn't work for the ! syntax
+    },
+    plugins: [
+        {
+            rules: {
+                "breaking-change-forbidden": ({
+                    body,
+                    footer,
+                    subject,
+                    type,
+                }) => {
+                    const hasBreakingFooter = /BREAKING[\-\ ]CHANGE/i.test(
+                        `${body ?? ""} ${footer ?? ""}`,
+                    );
+                    const hasBreakingType =
+                        type?.endsWith("!") || subject?.startsWith("!");
+
+                    return [
+                        !(hasBreakingFooter || hasBreakingType),
+                        "Breaking changes are not allowed on this branch",
+                    ];
+                },
+            },
+        },
+    ],
+};
